2833ae73d9d53df6f7215bd9276eca85d00bf182
Jailson <jjailson2004@gmail.com>
2022-07-05 15:39:41 -0300

    corrigindo o mui

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Head from 'next/head';
 import {useRouter} from 'next/router';
 import {AppContainer} from '../src/components/AppContainerComponent';
@@ -7,6 +7,12 @@ import {AppInput} from '../src/components/AppInputComponent';
 
 const Login = props => {
   const router = useRouter ();
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChangeName = (event) => {
+    setName(event.target.value);
+  }
 
   return (
     <div>
@@ -15,8 +21,18 @@ const Login = props => {
       </Head>
       <main>
         <AppContainer>
-          <AppInput label="Usuário" title="apelido ou email do usuário" />
-          <AppInput label="Senha" type="password" />
+          <AppInput 
+            label="Usuário" 
+            title="apelido ou email do usuário" 
+            value={name} 
+            onChange={handleChangeName}
+          />
+          <AppInput 
+            label="Senha" 
+            type="password" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <AppButton
             onClick={event => router.back ()}
             label="voltar"
@@ -25,6 +41,8 @@ const Login = props => {
           <AppButton
             title="Clique aqui para entrar no chat"
             label="Entrar no chat"
+            variant='contained'
+            disabled={!(!!name && !!password)}
           />
         </AppContainer>
       </main>
--- src/components/AppButtonContainer.js
+++ src/components/AppButtonContainer.js
@@ -2,16 +2,16 @@ import React from "react";
 import Button from "@mui/material/Button";
 
 export const AppButton = (props) => {
-  let href_button = props.href ? props.href : null;
   let color_button = props.color ? props.color : "primary";
   let handle_click = props.onClick ? props.onClick : (() => null);
+  let title_button = props.title ? props.title : "unamed";
 
   return (
     <Button
       color={color_button}
       onClick={handle_click}
       title={props.title}
-      href={href_button}
+      {...props}
     >
       {props.label}
     </Button>
