dba6d6c37c73725855fcb9dcef06e068de4b7cb5
Jailson <jjailson2004@gmail.com>
2022-07-12 16:12:41 -0300

    corrigindo docs

--- pages/_documents.js
+++ pages/_documents.js
@@ -8,28 +8,28 @@ class MyDocument extends Document {
         const initialProps = await Document.getInitialProps(ctx)
         return {... this.initialProps}
     }
+    render() {
+        return(
+            <Html>
+                <Head>
+                    <link
+                        href="https://fonts.googleapis.com/css?family=Roboto:300.400.500.700&display=swap"
+                        rel="stylesheet"
+                    />
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    
+                    />
+                </Head>
+                <body>
+                <Main />
+                <NextScript />
+                </body>
+            </Html>
+        )
+    }
 }
 
-render() {
-    return(
-        <Html>
-            <Head>
-                <link
-                    href="https://fonts.googleapis.com/css?family=Roboto:300.400.500.700&display=swap"
-                    rel="stylesheet"
-                />
-                <link
-                    rel="stylesheet"
-                    href="https://fonts.googleapis.com/icon?family=Material+Icons"
-
-                />
-            </Head>
-            <body>
-            <Main />
-            <NextScript />
-            </body>
-        </Html>
-    )
-}
 
 export default MyDocument
